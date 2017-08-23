994409e1284392f85ddf2379bc1941f4f24181a3
Aditya Bhargava <abhargv3@illinois.edu>
2017-08-22 20:52:02 -0500

    adding maze generation file

---
+++ Assets/Scripts/GameMan.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMan : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
